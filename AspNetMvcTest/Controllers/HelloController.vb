4c0bf36150398196a7510a7e0dd82feccba828e9
KazuoUrano00 <urano.kazuo@opst.co.jp>
2018-12-17 10:32:31 +0900

    変更

---
+++ AspNetMvcTest/Controllers/HelloController.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HelloController
+        Inherits Controller
+
+        ' GET: Hello
+        Function Index() As ActionResult
+            ViewData("Message") = "ASP.NET MVC へようこそ"
+            Return View()
+        End Function
+    End Class
+End Namespace
---
+++ AspNetMvcTest/Views/Hello/Index.vbhtml
@@ -0,0 +1,19 @@
+﻿
+@Code
+    Layout = Nothing
+End Code
+
+<!DOCTYPE html>
+
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>@ViewData("Message")</title>
+</head>
+<body>
+    <div>
+        @ViewData("Message")
+    </div>
+</body>
+</html>
